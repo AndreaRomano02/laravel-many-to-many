fa62a5bafa6d64232cabc794e2b4fb6467bbe887
AndreaRomano02 <andy.romano2002@gmail.com>
2023-09-07 14:20:07 +0200

    Typo fix migration

--- database/migrations/2023_09_07_113557_create_project_technology_table.php
+++ database/migrations/2023_09_07_113557_create_project_technology_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
    */
   public function up(): void
   {
-    Schema::create('project_tecnology', function (Blueprint $table) {
+    Schema::create('project_technology', function (Blueprint $table) {
       $table->id();
       $table->foreignId('project_id')->constrained()->onDelete('cascade');
       $table->foreignId('technology_id')->constrained()->onDelete('cascade');
