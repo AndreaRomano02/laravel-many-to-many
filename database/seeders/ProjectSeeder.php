0bddf5eecaa060fbd9f1270311793a8c118c3664
AndreaRomano02 <andy.romano2002@gmail.com>
2023-09-07 14:24:18 +0200

    Add to ProjectSeeder relationship with technology

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Technology;
 use App\Models\Type;
 use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -17,6 +18,7 @@ class ProjectSeeder extends Seeder
   public function run(Generator $faker): void
   {
     $types_ids = Type::pluck('id')->toArray();
+    $technology_ids = Technology::pluck('id')->toArray();
 
     for ($i = 0; $i < 50; $i++) {
       $project = new Project();
@@ -25,6 +27,12 @@ class ProjectSeeder extends Seeder
       $project->description = $faker->paragraphs(5, true);
       $project->type_id = Arr::random($types_ids);
       $project->save();
+      $project_technologies = [];
+      foreach ($technology_ids as $tech_id) {
+        if ($faker->boolean()) $project_technologies[] = $tech_id;
+      }
+
+      $project->technologies()->attach($project_technologies);
     }
   }
 }
